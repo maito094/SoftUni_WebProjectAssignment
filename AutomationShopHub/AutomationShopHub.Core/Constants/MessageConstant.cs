f4d908f30d243d82ff0ff72db43ccc0de761b297
Metodi Bozhkov <mlbozhkov@gmail.com>
2022-11-23 04:01:40 +0200

    Agent Become View and Front-End cosmetics with Toastr JQuery Lib.
    Creation and usage of Become Agent View with error visualization by including the Toastr library.

---
+++ AutomationShopHub/AutomationShopHub.Core/Constants/MessageConstant.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomationShopHub.Core.Constants
+{
+   public static class MessageConstant
+   {
+      public const string ErrorMessage = "ErrorMessage";
+      public const string WarningMessage = "WarningMessage";
+      public const string SuccessMessage = "SuccessMessage";
+   }
+}
--- AutomationShopHub/AutomationShopHub/wwwroot/js/site.js
+++ AutomationShopHub/AutomationShopHub/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+toastr.options = {
+   "closeButton": false,
+   "debug": false,
+   "newestOnTop": true,
+   "progressBar": false,
+   "positionClass": "toast-bottom-full-width",
+   "preventDuplicates": false,
+   "onclick": null,
+   "showDuration": "300",
+   "hideDuration": "1000",
+   "timeOut": "5000",
+   "extendedTimeOut": "1000",
+   "showEasing": "swing",
+   "hideEasing": "linear",
+   "showMethod": "fadeIn",
+   "hideMethod": "fadeOut"
+}
