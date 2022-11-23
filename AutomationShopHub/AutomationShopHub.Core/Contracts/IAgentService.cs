a2a8d5ff58df3fba94f87bab26f9bb6cd59cbe8b
Metodi Bozhkov <mlbozhkov@gmail.com>
2022-11-23 03:36:24 +0200

    IAgentService and AgentService Implementation
    Implement service for Sales Agent Tasks

---
+++ AutomationShopHub/AutomationShopHub.Core/Contracts/IAgentService.cs
@@ -0,0 +1,12 @@
+﻿namespace AutomationShopHub.Core.Contracts
+{
+   public interface IAgentService
+   {
+      Task<bool> ExistById(string userId);
+      Task<bool> UserWithPhoneExists(string phoneNumber);
+      Task<bool> UserWithEmailExists(string email);
+      Task<bool> UserHasActiveOrders(string userId);
+
+      Task Create(string userId, string phoneNumber, string? imageProfileUrl);
+   }
+}
---
+++ AutomationShopHub/AutomationShopHub.Core/Services/AgentService.cs
@@ -0,0 +1,59 @@
+﻿using AutomationShopHub.Core.Contracts;
+using AutomationShopHub.Infrastructure.Data.Common;
+using AutomationShopHub.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomationShopHub.Core.Services
+{
+   public class AgentService : IAgentService
+   {
+      private readonly IRepository repo;
+
+      public AgentService(IRepository _repo)
+      {
+         repo = _repo;
+      }
+
+      public async Task Create(string userId, string phoneNumber, string? imageProfileUrl)
+      {
+         var agent = new SalesAgent()
+         {
+            UserId = userId,
+            TelephoneNumber = phoneNumber
+         };
+
+         if (imageProfileUrl is not null)
+         {
+            agent.ImageProfileUrl = imageProfileUrl;
+         }
+
+         await repo.AddAsync(agent);
+         await repo.SaveChangesAsync();
+
+      }
+
+      public async Task<bool> ExistById(string userId)
+      {
+         return await repo.All<SalesAgent>()
+            .AnyAsync(a => a.UserId == userId);
+      }
+
+      public async Task<bool> UserHasActiveOrders(string userId)
+      {
+         return await repo.All<Order>()
+         .AnyAsync(o => !o.isConfirmed && o.SalesAgent.UserId == userId);
+      }
+
+      public async Task<bool> UserWithEmailExists(string email)
+      {
+         return await repo.All<SalesAgent>()
+            .AnyAsync(a => a.User.Email == email);
+      }
+
+      public async Task<bool> UserWithPhoneExists(string phoneNumber)
+      {
+         return await repo.All<SalesAgent>()
+            .AnyAsync(a => a.TelephoneNumber == phoneNumber);
+      }
+   }
+}
